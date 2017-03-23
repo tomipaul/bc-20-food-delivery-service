9f8c85754b00c5403620c824c77f9cedbb17ffad
Oluwatomi Akande <tomipaul95@gmail.com>
2017-03-23 15:26:04 +0100

    Make user view interactive with flow

--- public/js/foodview.js
+++ public/js/foodview.js
@@ -5,8 +5,6 @@ function getAllFoods(cb) {
   req.responseType = "json";
   req.send();
   req.onload = () => {
-      
-
     if (req.status === 500) {
       return cb(false);
     }
@@ -38,7 +36,6 @@ function addFood(cb) {
         if (req.status === 500) {
           return cb(false);
         }
-        displayAddFood()
         return cb(req.response);
       }
     }
@@ -48,13 +45,15 @@ function addFood(cb) {
   });
 }
 
-function displayAddFood() {
-  let state = document.getElementById("displayFormBg").style.display;
+function showPrompt(elemId) {
+  let state = document.getElementById(elemId).style.display;
   if (state === "block") {
-    document.getElementById("displayFormBg").style.display = "none";
+    document.getElementById(elemId).style.display = "none";
+    document.getElementById(elemId).parentNode.style.display = "none";
   }
   else {
-    document.getElementById("displayFormBg").style.display = "block";
+    document.getElementById(elemId).style.display = "block";
+    document.getElementById(elemId).parentNode.style.display = "block";
   }
 }
 
@@ -75,6 +74,7 @@ function displayFood(food) {
   let foodDiv = document.createElement("div");
   let foodImg = document.createElement('img');
   let foodName = document.createElement('div');
+  foodName.contentEditable = true;
   let orderImg = document.createElement('img');
   foodName.textContent = food.name;
   foodDiv.id = "food";
@@ -83,7 +83,7 @@ function displayFood(food) {
   foodDiv.appendChild(foodImg);
   foodDiv.appendChild(foodName);
   if (food.available) {
-    orderImg.src = "images/cart.png";
+    orderImg.src = "/images/cart.png";
     orderImg.id = "orderImg";
     orderImg.foodName = food.name;
     foodDiv.appendChild(orderImg);
@@ -92,21 +92,100 @@ function displayFood(food) {
 }
 
 function displayFoods(foods) {
-  const foods = JSON.parse(foods);
   for (const foodId in foods) {
     displayFood(foods[foodId]);
   }
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-      console.log('response');
+function makeOrder(cb) {
+  const foodName = document.getElementById("foodname").value;
+  const foodQuantity = document.getElementById("foodquantity").value;
+  const deliveryAddress = document.getElementById("deliveryaddress").value;
+  const customerName = document.getElementById("customername").value;
+  const req = new XMLHttpRequest();
+  const uri = '/api/order/';
+  const userEmail = firebase.auth().currentUser.email;
+  req.open('POST', uri, true);
+  const data = {
+    foodItem: foodName,
+    user: userEmail,
+    quantity: foodQuantity,
+    address: deliveryAddress,
+    deliverTo: customerName,
+    processed: false
+  };
+  req.setRequestHeader("Content-type", "application/json");
+  req.send(JSON.stringify(data));
+  req.onload = () => {
+    if (req.status === 201) {
+      return cb(true);
+    }
+    return cb(false);
+  }
+}
 
+function logOut() {
+  firebase.auth().signOut()
+  .then(() => {
+    document.cookie = `token=''; expires=''; path=\/`;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
   event.preventDefault();
-  getAllFoods((respons) => {
-    console.log(respons);
-    // if (response) {
-    //   console.log(response);
-    //   displayFoods(response);
-    // }
+  getAllFoods((response) => {
+    if (response) {
+      displayFoods(response);
+    }
+  });
+
+  document.getElementById("submitFoodInfo").addEventListener('click', (event) => {
+    event.preventDefault();
+    addFood((response) => {
+      showPrompt("displayFoodForm");
+      displayFood(JSON.parse(response));
+    });
+  });
+
+  document.getElementById("foods").addEventListener('click', (event) => {
+    event.preventDefault();
+    if (event.target && event.target.id === "orderImg") {
+      document.getElementById("foodname").value = event.target.foodName;
+      document.getElementById("foodname").disabled = true;
+      showPrompt("displayOrderForm");
+    }
+  });
+
+  document.getElementById("submitOrdersInfo").addEventListener('click', (event) => {
+    event.preventDefault();
+    makeOrder((res) => {
+      if (res) {
+        showPrompt("displayOrderForm");
+        alert("Order Successful");
+      }
+      else {
+        alert("Oops!Something went wrong");
+      }
+    });
+  });
+
+  window.addEventListener('load', (event)=>{
+    event.preventDefault();
+    const uid = firebase.auth().currentUser.uid;
+    const req = new XMLHttpRequest();
+    const uri = `/user/${uid}/false`;
+    req.open('GET', uri, true);
+    req.responseType = "json";
+    req.send();
+    req.onload = () => {
+      if (!req.response) {
+        alert("Error checking user status. Please try again!");
+      }
+      else {
+        document.getElementsByClassName("hide-elem").forEach((elem) => {
+          elem.classList.remove("hide-elem");
+        });
+      }
+    }
   });
 });
