01903b7aef59baacd713cf1dd73cff2515d07545
Oluwatomi Akande <tomipaul95@gmail.com>
2017-03-24 06:15:21 +0100

    Check auth state and status

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,12 +1,26 @@
-function checkUserState() {
+function checkUserState(cb) {
   firebase.auth().onAuthStateChanged((user) => {
     if (!user || !document.cookie.includes('token')) {
       location.assign('/');
     }
+    else {
+      return cb(user);
+    }
   });
 }
 
-window.addEventListener('load', (event) => {
-  event.preventDefault();
-  checkUserState();
-})
+function checkUserStatus(user, cb) {
+  const req = new XMLHttpRequest();
+  const uri = `/user/${user.uid}`;
+  req.open('GET', uri, true);
+  req.responseType = "json";
+  req.send();
+  req.onload = () => {
+    console.log(req.response);
+    return cb(req.response);
+      /*const elementList = document.getElementsByClassName("if-admin");
+      for (let elem = 0; elem < elementList.length; elem++) {
+        elementList.item(elem).classList.remove("hide-elem");
+      }*/
+  }
+}
